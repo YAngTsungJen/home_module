aa779c8f4bfc0e3b9a030a2580769bf6baecb36f
mike <q23362352@yhaoo.com.tw>
2021-08-13 00:09:57 +0800

    build

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top py-0 py-md-2">
+      <div class="container-fluid container-md px-0 px-md-2">
+        <h1 class="navbar-logo text-white">
+          新森活
+        </h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link scrollTop" href="#">建案簡介</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link scrollTop" href="#">建築賞心</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link scrollTop" href="#">周邊環境</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
